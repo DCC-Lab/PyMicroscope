62a65bb7b00a6c1fd868c38ce714c9413db5869e
Sara-Maude Gagnon <smgag8@ulaval.ca>
2025-05-26 12:53:38 -0400

    Update test_read_write_command.py
    updated test and validation of the right port path to communicate with video rate controller.
    
    Proper path on DCC_VIdeo-2P.local is : "/dev/cu.USA19QW3d1P1.1"
    3 commands were tested, more to come.

--- tests/test_read_write_command.py
+++ tests/test_read_write_command.py
@@ -5,61 +5,145 @@ from serial.tools import list_ports
 
 import time
 
-port_path = '/dev/cu.wlan-debug'
+CONTROLLER_SERIAL_PATH = "/dev/cu.USA19QW3d1P1.1"
 
 class TestReadWrite(unittest.TestCase):
     def test000_init(self):
         self.assertTrue(True)
 
-    def test005_list_ports(self):
+    def test003_list_ports(self):
 
         ports_list = serial.tools.list_ports.comports()
         self.assertIsNotNone(ports_list)
 
         ftdi_ports = []
         for port_info in ports_list:
-            if port_info.vid == 1027:
-                ftdi_ports.append(port_info)
+            if False:
+                print(f"Device:{port_info.device} vid:{port_info.vid} pid:{port_info.pid}")
 
-        self.assertTrue(len(ftdi_ports) != 0)
+        self.assertTrue(len(ports_list) > 0)
 
 
+    def test005_find_keyspan_port(self):
+        KEYSPAN_VID = 1741
+        ports_list = serial.tools.list_ports.comports()
+        self.assertIsNotNone(ports_list)
 
-        # print(f"FTDI Device:{port_info.device} vid:{port_info.vid} pid:{port_info.pid}")
+        keyspan_ports = []
+        for port_info in ports_list:
+            if port_info.vid == KEYSPAN_VID:
+                keyspan_ports.append(port_info)
 
-    @unittest.SkipTest
+        self.assertTrue(len(keyspan_ports) != 0)
+        for port_info in keyspan_ports:
+            print(f"Device:{port_info.device} vid:{port_info.vid} pid:{port_info.pid}")
+
+    def get_keyspan_ports(self):
+        KEYSPAN_VID = 1741
+        ports_list = serial.tools.list_ports.comports()
+
+        keyspan_ports = []
+        for port_info in ports_list:
+            if port_info.vid == KEYSPAN_VID:
+                keyspan_ports.append(port_info)
+
+        return keyspan_ports
+
     def test010_create_serial_port(self):
-        port = serial.Serial(port_path, baudrate=19200, timeout=3, )
-        self.assertIsNotNone(port)
+        possible_ports = self.get_keyspan_ports()
+
+        usable_ports = []
+        for port_info in possible_ports:
+            try:
+                port = serial.Serial(port_info.device, baudrate=19200, timeout=0.5)
+                usable_ports.append(port_info.device)
+            except:
+                pass            
+
+        self.assertTrue(len(usable_ports) > 0)
+
+    def get_usable_ports(self):
+        keyspan_ports = self.get_keyspan_ports()
+
+        usable_ports = []
+        for port_info in keyspan_ports:
+            try:
+                port = serial.Serial(port_info.device, baudrate=19200, timeout=0.5)
+                usable_ports.append(port_info.device)
+                port.close()
+            except:
+                pass            
+
+        return usable_ports
+        
+    def test015_validate_usable_ports(self):
+        usable_ports = self.get_usable_ports()
+        self.assertTrue(CONTROLLER_SERIAL_PATH in usable_ports)
+
+    def test020_read_firmware_version(self):
+        usable_ports = self.get_usable_ports()
+        self.assertTrue(CONTROLLER_SERIAL_PATH in usable_ports)
+
+        READ_FIRMWARE_VERSION_BYTES = [0x7f]
+
+        port = serial.Serial(CONTROLLER_SERIAL_PATH, baudrate=19200, timeout=0.5)
+        
+        port.write(READ_FIRMWARE_VERSION_BYTES)
+
+        firmware_version_data = port.read(3)
+        self.assertIsNotNone(firmware_version_data)
+
+        self.assertTrue(firmware_version_data[0] == 4)
+        self.assertTrue(firmware_version_data[1] == 0)
+        self.assertTrue(firmware_version_data[2] == 0)
+
+    def test030_read_cid(self):
+        usable_ports = self.get_usable_ports()
+        self.assertTrue(CONTROLLER_SERIAL_PATH in usable_ports)
+
+        READ_CID = [0x6c]
+
+        port = serial.Serial(CONTROLLER_SERIAL_PATH, baudrate=19200, timeout=0.5)
+        
+        port.write(READ_CID)
+        cid_data = port.read(2)
+        self.assertIsNotNone(cid_data)
+        self.assertTrue(cid_data[0] == 0)
+        self.assertTrue(cid_data[1] == 0)
+
+    def test040_read_cpn(self):
+        usable_ports = self.get_usable_ports()
+        self.assertTrue(CONTROLLER_SERIAL_PATH in usable_ports)
+
+        READ_CPN = [0x6d]
+
+        port = serial.Serial(CONTROLLER_SERIAL_PATH, baudrate=19200, timeout=0.5)
+        
+        port.write(READ_CPN)
+        cpn_data = port.read(2)
+        self.assertIsNotNone(cpn_data)
+        self.assertTrue(cpn_data[0] == 2)
+        self.assertTrue(cpn_data[1] == 10)
+
+    def test050_many_commands(self):
+        port = serial.Serial(CONTROLLER_SERIAL_PATH, baudrate=19200, timeout=0.5)
+
+        all_commands = { "READ_FIRMWARE_VERSION" : {"command_bytes":[0x7f], "bytes_returned":3},
+                         "READ_CID" : {"command_bytes":[0x6c], "bytes_returned":2},
+                         "READ_CPN" : {"command_bytes":[0x6d], "bytes_returned":2}
+                     }
+
+        for command_name, command_dict in all_commands.items():
+            command_bytes = command_dict['command_bytes']
+            bytes_returned = command_dict['bytes_returned']
+
+            port.write(command_bytes)
+            cpn_data = port.read(bytes_returned)
+            self.assertIsNotNone(cpn_data)
+            self.assertIsNotNone(len(cpn_data) == bytes_returned)
+            print(f"Testing {command_name}: returned {cpn_data}")
 
 
 if __name__ == "__main__":
     unittest.main()
-
-# # Attendre un peu pour que le dispositif envoie des données
-# time.sleep(2)
-
-# info = serial.Serial('/dev/cu.wlan-debug', baudrate=19200, timeout=3, ) # /dev/cu.Bluetooth-Incoming-Port ou /dev/cu.wlan-debug
-
-# # Initialiser un tableau de bytes pour stocker les données
-# data = bytearray()
-# # Lire des données jusqu'à ce qu'une nouvelle ligne soit rencontrée
-# while True:
-#     byte = info.read()  # Lire un byte
-#     if byte:  # Vérifier si un byte a été lu
-#         data += byte  # Ajouter le byte au tableau de données
-#         if byte == b'\n':  # Vérifier si le byte est une nouvelle ligne
-#             break  # Sortir de la boucle si une nouvelle ligne est rencontrée
-#     else:
-#         print("Aucune donnée reçue.")
-#         break  # Sortir de la boucle si aucune donnée n'est reçue
-
-# # Convertir les données en chaîne de caractères
-# string = data.decode('utf-8')  # Utiliser decode() pour convertir en chaîne
-# print(string)  # Afficher la chaîne'''
-
-# info.close()
-
-# #sauver dans terminale: ls /dev/cu.* 
-
 
