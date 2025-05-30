faead81e1fdb55da924f1a6f7ee64b08aeb3ceda
Sara-Maude Gagnon <smgag8@ulaval.ca>
2025-05-30 14:44:27 -0400

    finish the dictionnary and check that all test still working with my computer

--- tests/test_read_write_command.py
+++ tests/test_read_write_command.py
@@ -5,7 +5,8 @@ from serial.tools import list_ports
 
 import time
 
-CONTROLLER_SERIAL_PATH = "/dev/cu.USA19QW3d1P1.1"
+#CONTROLLER_SERIAL_PATH = "/dev/cu.USA19QW3d1P1.1"
+CONTROLLER_SERIAL_PATH = "/dev/cu.usbserial-A907SJ89"
 
 class TestReadWrite(unittest.TestCase):
     def test000_init(self):
@@ -15,17 +16,17 @@ class TestReadWrite(unittest.TestCase):
 
         ports_list = serial.tools.list_ports.comports()
         self.assertIsNotNone(ports_list)
+        #print(ports_list)
 
-        ftdi_ports = []
+        #ftdi_ports = []
         for port_info in ports_list:
-            if False:
-                print(f"Device:{port_info.device} vid:{port_info.vid} pid:{port_info.pid}")
+            #if False:
+            print(f"Device:{port_info.device} vid:{port_info.vid} pid:{port_info.pid}")
 
         self.assertTrue(len(ports_list) > 0)
 
-
     def test005_find_keyspan_port(self):
-        KEYSPAN_VID = 1741
+        KEYSPAN_VID = 1027          # ancienne valeur 1741
         ports_list = serial.tools.list_ports.comports()
         self.assertIsNotNone(ports_list)
 
@@ -39,7 +40,7 @@ class TestReadWrite(unittest.TestCase):
             print(f"Device:{port_info.device} vid:{port_info.vid} pid:{port_info.pid}")
 
     def get_keyspan_ports(self):
-        KEYSPAN_VID = 1741
+        KEYSPAN_VID = 1027          # ancienne valeur 1741
         ports_list = serial.tools.list_ports.comports()
 
         keyspan_ports = []
@@ -57,10 +58,12 @@ class TestReadWrite(unittest.TestCase):
             try:
                 port = serial.Serial(port_info.device, baudrate=19200, timeout=0.5)
                 usable_ports.append(port_info.device)
+            
             except:
                 pass            
 
         self.assertTrue(len(usable_ports) > 0)
+
 
     def get_usable_ports(self):
         keyspan_ports = self.get_keyspan_ports()
@@ -75,11 +78,11 @@ class TestReadWrite(unittest.TestCase):
                 pass            
 
         return usable_ports
-        
+  
     def test015_validate_usable_ports(self):
         usable_ports = self.get_usable_ports()
         self.assertTrue(CONTROLLER_SERIAL_PATH in usable_ports)
-
+    
     def test020_read_firmware_version(self):
         usable_ports = self.get_usable_ports()
         self.assertTrue(CONTROLLER_SERIAL_PATH in usable_ports)
@@ -92,11 +95,11 @@ class TestReadWrite(unittest.TestCase):
 
         firmware_version_data = port.read(3)
         self.assertIsNotNone(firmware_version_data)
-
+        
         self.assertTrue(firmware_version_data[0] == 4)
         self.assertTrue(firmware_version_data[1] == 0)
         self.assertTrue(firmware_version_data[2] == 0)
-
+    
     def test030_read_cid(self):
         usable_ports = self.get_usable_ports()
         self.assertTrue(CONTROLLER_SERIAL_PATH in usable_ports)
@@ -124,13 +127,24 @@ class TestReadWrite(unittest.TestCase):
         self.assertIsNotNone(cpn_data)
         self.assertTrue(cpn_data[0] == 2)
         self.assertTrue(cpn_data[1] == 10)
+    
 
     def test050_many_commands(self):
         port = serial.Serial(CONTROLLER_SERIAL_PATH, baudrate=19200, timeout=0.5)
 
         all_commands = { "READ_FIRMWARE_VERSION" : {"command_bytes":[0x7f], "bytes_returned":3},
                          "READ_CID" : {"command_bytes":[0x6c], "bytes_returned":2},
-                         "READ_CPN" : {"command_bytes":[0x6d], "bytes_returned":2}
+                         "READ_CPN" : {"command_bytes":[0x6d], "bytes_returned":2},
+                         "READ_SN" : {"command_bytes":[0x6b], "bytes_returned":2},
+                         "READ_EEPROM_ADDRESS" : {"command_bytes":[0x76], "bytes_returned":1},
+                         "READ_STATE_OF_SWITCHES_AND_TTL_IOS" : {"command_bytes":[0x7e], "bytes_returned":1},
+                         "READ_BUILD_TIME" : {"command_bytes":[0x6a], "bytes_returned":9},
+                         "READ_BUILD_DATE" : {"command_bytes":[0x69], "bytes_returned":11},
+                         "READ_TMR1_RELOAD" : {"command_bytes":[0x75], "bytes_returned":2},
+                         "READ_NUMBER_OF_LINES_PER_FRAME" : {"command_bytes":[0x74], "bytes_returned":2},
+                         "READ_DAC_START" : {"command_bytes":[0x73], "bytes_returned":2},
+                         "READ_DAC_INCREMENT" : {"command_bytes":[0x72], "bytes_returned":2},
+                         "READ_NUMBER_OF_LINES_FOR_VSYNC" : {"command_bytes":[0x6e], "bytes_returned":2}
                      }
 
         for command_name, command_dict in all_commands.items():
@@ -142,7 +156,7 @@ class TestReadWrite(unittest.TestCase):
             self.assertIsNotNone(cpn_data)
             self.assertIsNotNone(len(cpn_data) == bytes_returned)
             print(f"Testing {command_name}: returned {cpn_data}")
-
+            
 
 if __name__ == "__main__":
     unittest.main()
