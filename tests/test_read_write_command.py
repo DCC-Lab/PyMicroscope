e2fe5fd253e482d206dc8dfc14afe5c2317a3307
smgag8 <152213480+smgag8@users.noreply.github.com>
2025-06-30 10:51:43 -0400

    Translation from Matlab to Python for scanning controller  (#3)
    * first_version_reading_port
    
    * Begining code for the aquisition of byte and reading values, trying to connecte with the USB port
    
    * Displaced and renamed
    
    * Update test_read_write_command.py
    
    * Update test_read_write_command.py
    
    updated test and validation of the right port path to communicate with video rate controller.
    
    Proper path on DCC_VIdeo-2P.local is : "/dev/cu.USA19QW3d1P1.1"
    3 commands were tested, more to come.
    
    * finish the dictionnary and check that all test still working with my computer
    
    * better erreur management when not connected
    
    * beginning of decoding, test have to be done to continue on this part.
    
    * finalise the unpack and introducing calculate variable
    
    * ajustement after testing
    
    * polygon Clock frequency fonction, introducing the writing commande and other commande from mathlab code, completed the dictionnairy but the result have not be tested yet
    
    * ajustement, test for finding the TMR1_RELOAD problems
    
    * continue with the writing commande
    
    * continue writing
    
    * Create vrscdt.m
    
    * Removed inexistent commands
    
    Some commands are not needed at this point (reprogramming the EEPROM fro instance).
    
    * instertion of the last mathlab variable and cleaning code
    
    * Update test_read_write_command.py
    
    * Update test_read_write_command.py
    
    Changed test function names
    
    * Changed hexadec for paramter for readability
    
    * Updated tests to actually send command  code
    
    Original version did not send the actual command code, only the parameter. Fixed. Unfortunately, the device protocol does not return an error code or a status code.
    
    * Moved command definitions to setUp() in dictionary with default parameters
    
    * Now using a self.port variable
    
    * Changed command_bytes to command_code to be consistent to all commands
    
    * Using struct.pack for READ_* commands, like WRITE commands
    
    * Single function for READ and WRITE commands
    
    * Now calculates bytes_returned from response_bytes_format
    
    * Some tests fail: values are sometimes lagging behind.
    
    May be a buffer issue? Unclear.  To debug.
    
    * make all test working, the number of bites between the read and the write command wasn't the same. some proprity have be created
    
    * complet the read and write test fonction for the test_vmscontroller document
    
    * Build_info now returns all relevant information about the build
    
    * beggin raise text error
    
    * Reverted to no error: it is not an error to have incorrect lines or wrong CID
    
    * test implamantation commande and def buid_info
    
    ---------
    
    Co-authored-by: Sara-Maude Gagnon and Daniel Côté <dccote@cervo.ulaval.ca>

---
+++ tests/test_read_write_command.py
@@ -0,0 +1,260 @@
+""" Port USB """
+import unittest
+import serial
+import struct
+from serial.tools import list_ports
+import binascii
+import time
+
+# CONTROLLER_SERIAL_PATH = "/dev/cu.USA19QW3d1P1.1"
+CONTROLLER_SERIAL_PATH = "/dev/cu.usbserial-A907SJ89"
+
+
+class TestReadWrite(unittest.TestCase):
+    def setUp(self):
+        self.default_write_parameters = {
+            "WRITE_DAC_START": 19200,
+            "WRITE_DAC_INCREMENT": 32,
+            "WRITE_NUMBER_OF_LINES_FOR_VSYNC": 6,
+            "WRITE_NUMBER_OF_LINES_PER_FRAME": 576,
+        }
+
+        self.commands = {
+            "READ_FIRMWARE_VERSION": {
+                "command_code": 0x7F,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "3b",
+            },
+            "READ_CID": {
+                "command_code": 0x6C,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "2b",
+            },
+            "READ_CPN": {
+                "command_code": 0x6D,
+                "command_bytes_format": ">b",
+                "response_bytes_format": ">h",
+            },
+            "READ_SN": {
+                "command_code": 0x6B,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "2b",
+            },
+            "READ_STATE_OF_SWITCHES_AND_TTL_IOS": {
+                "command_code": 0x7E,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "B",
+            },
+            "READ_BUILD_TIME": {
+                "command_code": 0x6A,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "8cx",
+            },
+            "READ_BUILD_DATE": {
+                "command_code": 0x69,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "11c",
+            },
+            "READ_NUMBER_OF_LINES_PER_FRAME": {
+                "command_code": 0x74,
+                "command_bytes_format": ">b",
+                "response_bytes_format": ">h",
+            },
+            "READ_DAC_START": {
+                "command_code": 0x73,
+                "command_bytes_format": ">b",
+                "response_bytes_format": ">h",
+            },
+            "READ_DAC_INCREMENT": {
+                "command_code": 0x72,
+                "command_bytes_format": ">b",
+                "response_bytes_format": ">h",
+            },
+            "READ_NUMBER_OF_LINES_FOR_VSYNC": {
+                "command_code": 0x6E,
+                "command_bytes_format": ">b",
+                "response_bytes_format": ">h",
+            },
+            "WRITE_DAC_START": {
+                "command_code": 0x7B,
+                "command_bytes_format": ">bh",
+                "response_bytes_format": "",
+                "parameter": self.default_write_parameters["WRITE_DAC_START"],
+            },
+            "WRITE_DAC_INCREMENT": {
+                "command_code": 0x7A,
+                "command_bytes_format": ">bh",
+                "response_bytes_format": "",
+                "parameter": self.default_write_parameters["WRITE_DAC_INCREMENT"],
+            },
+            "WRITE_NUMBER_OF_LINES_FOR_VSYNC": {
+                "command_code": 0x6F,
+                "command_bytes_format": ">bb",
+                "response_bytes_format": "",
+                "parameter": self.default_write_parameters[
+                    "WRITE_NUMBER_OF_LINES_FOR_VSYNC"
+                ],
+            },
+            "WRITE_NUMBER_OF_LINES_PER_FRAME": {
+                "command_code": 0x7C,
+                "command_bytes_format": ">bh",
+                "response_bytes_format": "",
+                "parameter": self.default_write_parameters[
+                    "WRITE_NUMBER_OF_LINES_PER_FRAME"
+                ],
+            },
+        }
+
+        self.port = serial.Serial(CONTROLLER_SERIAL_PATH, baudrate=19200, timeout=3)
+        self.port.reset_input_buffer()
+        self.port.reset_output_buffer()
+
+    def tearDown(self):
+        if self.port is not None:
+            self.port.close()
+
+    def test000_init(self):
+        self.assertTrue(True)
+
+    @unittest.SkipTest
+    def test003_list_ports(self):
+        ports_list = serial.tools.list_ports.comports()
+        self.assertIsNotNone(ports_list)
+
+        for port_info in ports_list:
+            print(f"Device:{port_info.device} vid:{port_info.vid} pid:{port_info.pid}")
+
+        self.assertTrue(len(ports_list) > 0)
+
+    @unittest.SkipTest
+    def test005_find_keyspan_port(self):
+        KEYSPAN_VID = 1027  # ancienne valeur 1741
+        ports_list = serial.tools.list_ports.comports()
+        self.assertIsNotNone(ports_list)
+
+        keyspan_ports = []
+        for port_info in ports_list:
+            if port_info.vid == KEYSPAN_VID:
+                keyspan_ports.append(port_info)
+
+        self.assertTrue(len(keyspan_ports) != 0)
+        for port_info in keyspan_ports:
+            print(f"Device:{port_info.device} vid:{port_info.vid} pid:{port_info.pid}")
+
+    def get_keyspan_ports(self):
+        KEYSPAN_VID = 1027  # ancienne valeur 1741
+        ports_list = serial.tools.list_ports.comports()
+
+        keyspan_ports = []
+        for port_info in ports_list:
+            if port_info.vid == KEYSPAN_VID:
+                keyspan_ports.append(port_info)
+
+        return keyspan_ports
+
+    def test010_create_serial_port(self):
+        possible_ports = self.get_keyspan_ports()
+
+        usable_ports = []
+        for port_info in possible_ports:
+            try:
+                port = serial.Serial(port_info.device, baudrate=19200, timeout=3)
+                usable_ports.append(port_info.device)
+            except:
+                pass
+
+        self.assertTrue(len(usable_ports) > 0)
+
+    def get_usable_ports(self):
+        keyspan_ports = self.get_keyspan_ports()
+
+        usable_ports = []
+        for port_info in keyspan_ports:
+            try:
+                port = serial.Serial(port_info.device, baudrate=19200, timeout=3)
+                usable_ports.append(port_info.device)
+                port.close()
+            except:
+                pass
+
+        return usable_ports
+
+    def test015_validate_usable_ports(self):
+        usable_ports = self.get_usable_ports()
+        self.assertTrue(CONTROLLER_SERIAL_PATH in usable_ports)
+
+    @unittest.skip
+    def test_060_polygonClockFrequency(self):
+        READ_TMR1_RELOAD = [0x75]
+        globalTMR1ReloadValue = 60327
+
+        self.port.write(READ_TMR1_RELOAD)  # bizare, à revoir il devrait y en avoir 2
+        data_bytes = self.port.read(2)
+        self.assertIsNotNone(data_bytes)
+        # self.assertEqual(len(data_bytes), 2)
+        part_number = struct.unpack("b", data_bytes)[0]  # devrait être <h
+        self.assertTrue(part_number == 0)
+
+        TMR1ReloadValueMinimum = 40535
+        TMR1ReloadValueMaximum = 60327
+        TMR1ReloadValueMostSignificantByte = part_number / 256
+        TMR1ReloadValueLeastSignificantByte = part_number % 256
+
+        polygonClockFrequency = 5000000 / (65535 - part_number)
+        # self.assertTrue(polygonClockFrequency == pass)
+        if polygonClockFrequency is True:
+            minimumiPhotonNumberOfPixelsSupported = 1
+            maximumiPhotonNumberOfPixelsSupported = 262144
+            iPhotonNumberOfPixelsPerLine = (
+                None  # we don't know his value with the mathlab code
+            )
+            # iPhotonRT 1.1 beta14 has an upper limit of 2048 pixels
+
+            numberOfFacesOfPolygon = 36
+            polygonRevolutionsPerMinute = polygonClockFrequency / 2 * 60
+            HSyncFrequency = polygonRevolutionsPerMinute * numberOfFacesOfPolygon
+            self.assertIsNotNone(HSyncFrequency)
+            pixelFrequency = iPhotonNumberOfPixelsPerLine * HSyncFrequency
+
+            maximumPixelFrequency = 20e6
+            self.assertTrue(maximumPixelFrequency >= pixelFrequency)
+            if pixelFrequency > maximumPixelFrequency:
+                raise ValueError("The frequence is to hig")
+            else:
+                pass
+
+            numberOfLinesPerFrame = 576
+            VSyncFrequency = HSyncFrequency / numberOfLinesPerFrame
+            self.assertIsNotNone(VSyncFrequency)
+
+            # Polygon clock: %0.1f Hz, HSync: %0.0f Hz, VSync %0.1f Hz, pixel frequency %0.2e Hz
+
+    def test_070_read_write_commands(self):
+        for command_name, command_dict in self.commands.items():
+            command_code = command_dict["command_code"]
+            command_bytes_format = command_dict["command_bytes_format"]
+            parameter = command_dict.get("parameter")
+
+            if parameter is not None:
+                payload = struct.pack(command_bytes_format, command_code, parameter)
+            else:
+                payload = struct.pack(command_bytes_format, command_code)
+
+            self.port.write(payload)
+
+            response_bytes_format = command_dict["response_bytes_format"]
+            bytes_returned = struct.calcsize(response_bytes_format)
+            if bytes_returned != 0:
+                response_bytes = self.port.read(bytes_returned)
+                self.assertEqual(
+                    len(response_bytes),
+                    bytes_returned,
+                    f"Response wrong length for {command_name}",
+                )
+                unpacked_response = struct.unpack(response_bytes_format, response_bytes)
+                self.assertIsNotNone(unpacked_response)
+                # print(f"{command_name}, {unpacked_response}")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/vmscontroller.py
@@ -0,0 +1,185 @@
+""" Port USB """
+import serial
+import struct
+from serial.tools import list_ports
+import binascii
+import time
+
+# CONTROLLER_SERIAL_PATH = "/dev/cu.USA19QW3d1P1.1"
+CONTROLLER_SERIAL_PATH = "/dev/cu.usbserial-A907SJ89"
+
+
+class VMSController:
+    def __init__(self):
+        self.default_write_parameters = {
+            "WRITE_DAC_START": 19200,
+            "WRITE_DAC_INCREMENT": 32,
+            "WRITE_NUMBER_OF_LINES_FOR_VSYNC": 6,
+            "WRITE_NUMBER_OF_LINES_PER_FRAME": 576,
+        }
+
+        self.commands = {
+            "READ_FIRMWARE_VERSION": {
+                "command_code": 0x7F,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "3b",
+            },
+            "READ_CID": {
+                "command_code": 0x6C,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "2b",
+            },
+            "READ_CPN": {
+                "command_code": 0x6D,
+                "command_bytes_format": ">b",
+                "response_bytes_format": ">h",
+            },
+            "READ_SN": {
+                "command_code": 0x6B,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "2b",
+            },
+            "READ_STATE_OF_SWITCHES_AND_TTL_IOS": {
+                "command_code": 0x7E,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "B",
+            },
+            "READ_BUILD_TIME": {
+                "command_code": 0x6A,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "8cx",
+            },
+            "READ_BUILD_DATE": {
+                "command_code": 0x69,
+                "command_bytes_format": ">b",
+                "response_bytes_format": "11c",
+            },
+            "READ_NUMBER_OF_LINES_PER_FRAME": {
+                "command_code": 0x74,
+                "command_bytes_format": ">b",
+                "response_bytes_format": ">h",
+            },
+            "READ_DAC_START": {
+                "command_code": 0x73,
+                "command_bytes_format": ">b",
+                "response_bytes_format": ">h",
+            },
+            "READ_DAC_INCREMENT": {
+                "command_code": 0x72,
+                "command_bytes_format": ">b",
+                "response_bytes_format": ">h",
+            },
+            "READ_NUMBER_OF_LINES_FOR_VSYNC": {
+                "command_code": 0x6E,
+                "command_bytes_format": ">b",
+                "response_bytes_format": ">h",
+            },
+            "WRITE_DAC_START": {
+                "command_code": 0x7B,
+                "command_bytes_format": ">bh",
+                "response_bytes_format": "",
+                "parameter": self.default_write_parameters["WRITE_DAC_START"],
+            },
+            "WRITE_DAC_INCREMENT": {
+                "command_code": 0x7A,
+                "command_bytes_format": ">bh",
+                "response_bytes_format": "",
+                "parameter": self.default_write_parameters["WRITE_DAC_INCREMENT"],
+            },
+            "WRITE_NUMBER_OF_LINES_FOR_VSYNC": {
+                "command_code": 0x6F,
+                "command_bytes_format": ">bh",
+                "response_bytes_format": "",
+                "parameter": self.default_write_parameters[
+                    "WRITE_NUMBER_OF_LINES_FOR_VSYNC"
+                ],
+            },
+            "WRITE_NUMBER_OF_LINES_PER_FRAME": {
+                "command_code": 0x7C,
+                "command_bytes_format": ">bh",
+                "response_bytes_format": "",
+                "parameter": self.default_write_parameters[
+                    "WRITE_NUMBER_OF_LINES_PER_FRAME"
+                ],
+            },
+        }
+
+        self.port = None
+
+    def initialize(self):
+        self.port = serial.Serial(CONTROLLER_SERIAL_PATH, baudrate=19200, timeout=3)
+
+        version = self.send_command("READ_FIRMWARE_VERSION")
+        if version[0] != 4:
+            raise RuntimeError("Unrecognized firmware version on controller")
+
+        #print(self.build_info())
+
+    def shutdown(self):
+        if self.port is not None:
+            self.port.close()
+
+    def build_info(self):
+        fw = self.send_command("READ_FIRMWARE_VERSION")
+        cid = self.send_command("READ_CID")
+        cpn = self.send_command("READ_CPN")
+        serial_number = self.send_command("READ_SN")
+        build_time = self.send_command("READ_BUILD_TIME")
+        build_date = self.send_command("READ_BUILD_DATE")
+        return f"VMS Controller : ({cid}, {cpn}, #{serial_number}), {fw[0]}.{fw[1]}.{fw[2]} [Build: {build_date}, {build_time}]\n"
+
+    def send_command(self, command_name, parameter=None):
+        command_dict = self.commands[command_name]
+
+        command_code = command_dict["command_code"]
+        command_bytes_format = command_dict["command_bytes_format"]
+
+        if parameter is not None:
+            payload = struct.pack(command_bytes_format, command_code, parameter)
+        else:
+            payload = struct.pack(command_bytes_format, command_code)
+
+        self.port.write(payload)
+        self.port.flush()
+        
+
+        response_bytes_format = command_dict["response_bytes_format"]
+        bytes_returned = struct.calcsize(response_bytes_format)
+        unpacked_response = None
+        if bytes_returned != 0:
+            response_bytes = self.port.read(bytes_returned)
+            unpacked_response = struct.unpack(response_bytes_format, response_bytes)
+
+        return unpacked_response
+
+    @property
+    def lines_per_frame(self):
+        return self.send_command("READ_NUMBER_OF_LINES_PER_FRAME")[0]
+
+    @lines_per_frame.setter
+    def lines_per_frame(self, value):
+        self.send_command("WRITE_NUMBER_OF_LINES_PER_FRAME", value)
+
+    @property
+    def lines_for_vsync(self):
+        return self.send_command("READ_NUMBER_OF_LINES_FOR_VSYNC")[0]
+
+    @lines_for_vsync.setter
+    def lines_for_vsync(self, value):
+        self.send_command("WRITE_NUMBER_OF_LINES_FOR_VSYNC", value)
+
+    @property
+    def dac_start(self):
+        return self.send_command("READ_DAC_START")[0]
+
+    @dac_start.setter
+    def dac_start(self, value):
+        self.send_command("WRITE_DAC_START", value)
+
+    @property
+    def dac_increment(self):
+        return self.send_command("READ_DAC_INCREMENT")[0]
+
+    @dac_increment.setter
+    def dac_increment(self, value):
+        self.send_command("WRITE_DAC_INCREMENT", value)
