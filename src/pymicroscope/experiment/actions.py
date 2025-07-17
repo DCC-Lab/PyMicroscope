fee03c5ba44df7cd4f7e4c5c2786a954cbfc6677
Daniel Côté <dccote@cervo.ulaval.ca>
2025-07-17 19:50:29 -0400

    Improvements on nomencalture and design
    Experiment is getting beefed up with new functions

--- src/pymicroscope/experiment/actions.py
+++ src/pymicroscope/experiment/actions.py
@@ -2,102 +2,192 @@ import time
 from pathlib import Path
 from dataclasses import dataclass
 from typing import Any, Optional
+import platform
+import os
 
-from mytk import App
+from mytk.notificationcenter import Notification, NotificationCenter
+import numpy as np
 
 from hardwarelibrary.motion import LinearMotionDevice
 
-
 class Action:
-    def __init__(self, inputs:dict[str, Any], *args, **kwargs):
-        self.inputs:dict[str, Any] = inputs
-        self.outputs:dict[str, Any] = None
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    def perform(self):
-        raise RuntimeError("You must implement the perform method in your class")
+    def perform(self, results=None):
+        start_time = time.time()
 
-@dataclass
-class ExperimentStep:
-    prepare_actions:list[Action] = None
-    perform_actions:list[Action] = None
-    finalize_actions:list[Action] = None
-    
-    def perform(self):
-        if self.prepare_actions is not None:
-            result = None
-            for action in self.prepare_actions:
-                result = action.perform(inputs=result)
+        results = self.do_perform(results)
+        if results is None:
+            results = {}
 
-        if self.perform_actions is not None:
-            result = None
-            for action in self.perform_actions:
-                result = action.perform(result=result)
-                
-        if self.finalize_actions is not None:
-            result = None
-            for action in self.finalize_actions:
-                result = action.perform(result=result)
+        results["start_time"] = start_time
+        results["duration"] = time.time() - start_time
+        results["action"] = self
+        return results
 
-            
+    def do_perform(self, results=None) -> dict[str, Any] | None:
+        raise RuntimeError(
+            "You must implement the do_perform method in your class"
+        )
+
 
 class ActionWait(Action):
     def __init__(self, delay, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.delay = delay
-        
-    def perform(self) -> Any | None:
+
+    def do_perform(self, results=None) -> dict[str, Any] | None:
         time.sleep(self.delay)
-    
-        
-class ActionMove(Action):
-    def __init__(self, position:list[int], linear_motion_device:LinearMotionDevice, *args, **kwargs):
+
+
+class ActionBell(Action):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.position:list[int] = position
-        self.device:LinearMotionDevice = linear_motion_device
-        
-    def perform(self) -> Any | None:
+
+    def do_perform(self, results=None) -> dict[str, Any] | None:
+        if platform.system() == "Darwin":
+            os.system("afplay /System/Library/Sounds/Glass.aiff")
+        else:
+            print("\a")
+
+
+class ActionMove(Action):
+    def __init__(
+        self,
+        position: list[int],
+        linear_motion_device: LinearMotionDevice,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.position: list[int] = position
+        self.device: LinearMotionDevice = linear_motion_device
+
+    def do_perform(self, results=None) -> dict[str, Any] | None:
         self.device.moveInMicronsTo(self.position)
+        return {"position": self.position}
+
 
 class ActionMoveBy(Action):
-    def __init__(self, d_position:list[int], linear_motion_device:LinearMotionDevice, *args, **kwargs):
+    def __init__(
+        self,
+        d_position: list[int],
+        linear_motion_device: LinearMotionDevice,
+        *args,
+        **kwargs,
+    ):
         super().__init__(*args, **kwargs)
-        self.d_position:list[int] = d_position
-        self.device:LinearMotionDevice = linear_motion_device
-        
-    def perform(self) -> Any | None:
-        self.device.moveInMicronsBy(self.d_position)
+        self.d_position: list[int] = d_position
+        self.device: LinearMotionDevice = linear_motion_device
 
+    def do_perform(self, results=None) -> dict[str, Any] | None:
+        self.device.moveInMicronsBy(self.d_position)
+        return {"displacement": self.d_position}
 
 
 class ActionCapture(Action):
-    def __init__(self, n_images, *args, **kwargs):
+    def __init__(self, n_images, queue, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.n_images = n_images
-        self.images = []
-    
+        self.queue = queue
+
+    def do_perform(self, results=None) -> dict[str, Any] | None:
+        index = 0
+        img_arrays = []
+
+        queue = results.get("save_queue")
+        if queue is None:
+            raise RuntimeError("No save queue available")
+
+        while index < self.n_images:
+            img_array = queue.get()
+            img_arrays.append(img_array)
+            index = index + 1
+
+        return {"captured_frames": img_arrays}
+
+
+class ActionMean(Action):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def do_perform(self, results=None) -> dict[str, Any] | None:
+        img_arrays = results.get("captured_frames")
+
+        stacked = np.stack(img_arrays).astype(np.float64)
+        mean_img = np.mean(stacked, axis=0)
+
+        return {"processed_frames": mean_img}
+
+
 class ActionSave(Action):
-    def __init__(self, filepath:Path, *args, **kwargs):
+    def __init__(self, filepath: Path, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.filepath = filepath
-        
-        
-class ExperimentManager:
-    def __init__(self, *args, **kwargs):
-        self.steps:list[ExperimentStep] = []
 
-    def add_action(self, action:Action):
-        self.steps.append( ExperimentStep(perform_actions=[Action]))
-    
-    def add_actions(self, actions:list[Action]):
+    def do_perform(self, results=None) -> dict[str, Any] | None:
+        pil_image = PILImage.fromarray(mean_img.astype(np.uint8), mode="RGB")
+        params["i"] = "avg"
+        filepath = self.root_dir / Path(self.template.format(**params))
+        pil_image.save(filepath)
+
+
+class ExperimentStep:
+    def __init__(
+        self,
+        prepare: list[Action] = None,
+        perform: list[Action] = None,
+        finalize: list[Action] = None,
+        *args,
+        **kwargs,
+    ):
+        self.prepare_actions: list[Action] = prepare
+        self.perform_actions: list[Action] = perform
+        self.finalize_actions: list[Action] = finalize
+        self.results = {}
+
+    def perform(self, results=None):
+        if self.prepare_actions is not None:
+            for i, action in enumerate(self.prepare_actions):
+                result = action.perform(results=self.results)
+                if result is not None:
+                    self.results[f"prepare-{i}"] = result
+
+        if self.perform_actions is not None:
+            for i, action in enumerate(self.perform_actions):
+                result = action.perform(results=self.results)
+                if result is not None:
+                    self.results[f"perform-{i}"] = result
+
+        if self.finalize_actions is not None:
+            for i, action in enumerate(self.finalize_actions):
+                result = action.perform(results=self.results)
+                if result is not None:
+                    self.results[f"finalize-{i}"] = result
+
+        return self.results
+
+
+class Experiment:
+    def __init__(self, *args, **kwargs):
+        self.results = {}
+        self.steps: list[ExperimentStep] = []
+
+    def add_step(self, experiment_step):
+        self.steps.append(experiment_step)
+
+    def add_single_action_step(self, action: Action):
+        self.add_step(ExperimentStep(perform=[action]))
+
+    def add_many_single_action_steps(self, actions: list[Action]):
         for action in actions:
-            self.add_action(action=action)
-    
-    def perform_all_actions(self) -> Any | None:
-        for step in self.steps:
-            if step.prepare is not None:
-                for action in step.prepare:
-                    action.pe
-            
-            App.app.save()
-        
-        self.actions = []
+            self.add_single_action_step(action=action)
+
+    def perform(self) -> Any | None:
+        experiment_results = {}
+        for i, step in enumerate(self.steps):
+            results = step.perform()
+            experiment_results[f"step-{i}"] = results
+
+        return experiment_results
---
+++ tests/test_actions.py
@@ -0,0 +1,82 @@
+import envtest  # setup environment for testing
+from pymicroscope.experiment.actions import *
+import json
+
+
+class TestDevice:
+    def moveInMicronsTo(self, position):
+        pass
+
+    def moveInMicronsBy(self, distance):
+        pass
+
+
+class ActionTestCase(
+    envtest.CoreTestCase
+):  # pylint: disable=too-many-public-methods
+    def test000_init(self):
+        self.assertIsNotNone(Experiment())
+
+    def test010_action_move(self):
+        action = ActionMove(
+            position=(0, 0, 0), linear_motion_device=TestDevice()
+        )
+        self.assertIsNotNone(action)
+
+        with self.assertRaises(TypeError):
+            ActionMove(position=(0, 0, 0))
+
+    def test020_action_move(self):
+        action = ActionMove(
+            position=(0, 0, 0), linear_motion_device=TestDevice()
+        )
+        action.perform([{"Test": True}])
+
+    def test030_action_move_results(self):
+        action = ActionMove(
+            position=(0, 0, 0), linear_motion_device=TestDevice()
+        )
+        _ = action.perform({"Test": True})
+
+    def test040_exp_add_actions(self):
+        exp = Experiment()
+        exp.add_single_action_step(ActionWait(0.05))
+        exp.add_single_action_step(ActionWait(0.05))
+        exp.perform()
+
+    def test040_exp_add_actions(self):
+        exp = Experiment()
+        exp.add_single_action_step(ActionWait(0.05))
+        exp.add_single_action_step(ActionWait(0.05))
+        exp.perform()
+
+    def test050_exp_add_many_actions(self):
+        exp = Experiment()
+        exp.add_many_single_action_steps(
+            [ActionWait(0.02), ActionBell(), ActionWait(0.02)]
+        )
+        print(exp.perform())
+
+    def test060_exp_add_step(self):
+        exp = Experiment()
+        exp.add_step(
+            ExperimentStep(
+                prepare=[ActionWait(0.02), ActionBell(), ActionWait(0.02)],
+                perform=[ActionWait(0.02), ActionBell(), ActionWait(0.02)],
+                finalize=[ActionWait(0.02), ActionBell(), ActionWait(0.02)],
+            )
+        )
+        exp.add_step(
+            ExperimentStep(
+                prepare=[ActionWait(0.02), ActionBell(), ActionWait(0.02)],
+                perform=[ActionWait(0.02), ActionBell(), ActionWait(0.02)],
+                finalize=[ActionWait(0.02), ActionBell(), ActionWait(0.02)],
+            )
+        )
+
+        results = exp.perform()
+        # print(json.dumps(results, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    envtest.main()
