4f1a3b62d482e8d787aeb5c5e2b548d14e2b544a
Daniel Côté <dccote@cervo.ulaval.ca>
2025-08-07 11:50:42 -0400

    Update myMicroscope.py

--- src/pymicroscope/myMicroscope.py
+++ src/pymicroscope/myMicroscope.py
@@ -340,7 +340,7 @@ class MicroscopeApp(App):
         notif_complete = ActionPostNotification(
             MicroscopeAppNotification.did_save
         )
-        bell = ActionSound()
+        bell = ActionSound(sound_name=ActionSound.MacOSSound.FUNK)
         ending1 = ActionChangeProperty(self.save_button, "is_disabled", False)
         ending2 = ActionChangeProperty(
             self.number_of_images_average, "is_disabled", False
